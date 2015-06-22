e639da9bac787fdfb49e00134b4149402879953f
rustyjosh <rustyjosh@users.noreply.github.com>
2015-06-22 16:08:50 -0400

    core now has functions that call functions

--- src/ai_modern_approach/core.clj
+++ src/ai_modern_approach/core.clj
@@ -1,6 +1,17 @@
 (ns ai-modern-approach.core)
 
-(defn -main 
-  "I don't do a whole lot."
+(defn samplefunction 
+  { :doc "I don't do a whole lot."}
   []
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println ( + 1 1 ))
+  ( + 1 1 )
+)
+
+(defn -main
+  "Main"
+  []
+  ( def x (samplefunction))
+  ( println (str x "ez2win"))
+)
+
